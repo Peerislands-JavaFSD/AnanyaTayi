7f88fa4287cbde20db50de14d328acfd1e7a3d85
Ananyatayi <157120268+Ananyatayi@users.noreply.github.com>
2026-04-20 18:11:17 +0530

    Java based

---
+++ payroll_managment-java/src/main/java/org/revature/payroll/AppConfig.java
@@ -0,0 +1,28 @@
+package org.revature.payroll;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public TaxCalculator taxCalculator() {
+        return new IncomeTaxCalculator();
+    }
+
+    @Bean
+    public BonusScheme bonusScheme() {
+        return new PerformanceBonus();
+    }
+
+    @Bean
+    public PayrollProcessor payrollProcessor() {
+        PayrollProcessor processor =
+                new PayrollProcessor(taxCalculator(), "TechCorp Solutions", 2026);
+
+        processor.setBonusScheme(bonusScheme());
+
+        return processor;
+    }
+}
---
+++ payroll_managment-java/src/main/java/org/revature/payroll/IncomeTaxCalculator.java
@@ -0,0 +1,9 @@
+package org.revature.payroll;
+
+public class IncomeTaxCalculator implements TaxCalculator {
+
+    @Override
+    public double calculateTax(double salary) {
+        return salary * 0.20;
+    }
+}
